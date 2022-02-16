f58a45148f46d6c87118d135695a94c6fce8400a
demarche1 <alessandro.demarche00@gmail.com>
2022-02-16 19:48:14 -0300

    initial front end setup/register store config

---
+++ facebook-frontend/store/users/register.ts
@@ -0,0 +1,50 @@
+import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
+import { $axios } from "@/utils/nuxt-instance"
+import { User } from "@/models"
+
+interface CreatePayload {
+	redirectUrl: string,
+	email: string
+}
+
+interface ShowPayload {
+	key: string
+}
+
+interface updatePayload {
+	key: string,
+	name: string,
+	password: string,
+	passwordConfirmation: string
+}
+
+@Module({ name: 'users/register', stateFactory: true, namespaced: true })
+export default class UserRegister extends VuexModule {
+  private user = {} as User
+
+	get $user () {
+		return this.user
+	}
+
+  @Mutation
+  private UPDATE_USER(user: User) {
+    this.user = user
+  }
+
+  @Action
+  public async create(payload: CreatePayload) {
+    await $axios.$post('/users/register', payload)
+  }
+
+  @Action
+  public async show({ key }: ShowPayload) {
+    const user = await $axios.$post(`/users/register/${key}`)
+
+    this.context.commit('UPDATE_USER', user)
+  }
+
+  @Action
+  public async update(payload: updatePayload) {
+		await $axios.$post(`/users/register`, payload)
+  }
+}
