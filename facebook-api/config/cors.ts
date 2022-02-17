2f6b66f77a2563825a04a977a35fdc4473c78513
demarche1 <alessandro.demarche00@gmail.com>
2022-02-17 19:26:20 -0300

    allow cors

--- facebook-api/config/cors.ts
+++ facebook-api/config/cors.ts
@@ -20,7 +20,7 @@ const corsConfig: CorsConfig = {
   | you can define a function to enable/disable it on per request basis as well.
   |
   */
-  enabled: false,
+  enabled: true,
 
   // You can also use a function that return true or false.
   // enabled: (request) => request.url().startsWith('/api')
@@ -103,7 +103,7 @@ const corsConfig: CorsConfig = {
     'content-type',
     'expires',
     'last-modified',
-    'pragma',
+    'pragma'
   ],
 
   /*
@@ -128,7 +128,7 @@ const corsConfig: CorsConfig = {
   | https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age
   |
   */
-  maxAge: 90,
+  maxAge: 90
 }
 
 export default corsConfig
